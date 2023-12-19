4b97d5745c9f50d070300e60b70cac44f40878ae
LocTM <tranminhloc.anhk@gmail.com>
2023-12-19 10:20:19 +0700

    refactor case 1/1/2018

--- src/NextDayCalculator.java
+++ src/NextDayCalculator.java
@@ -1,8 +1,10 @@
 public class NextDayCalculator {
 
+    public static final String CONCATENATION = "/";
+
     public static String getNextDay(int dayTest, int monthTest, int yearTest) {
         int nextDay = dayTest+1;
-        String result = nextDay+"/"+monthTest+"/"+yearTest;
+        String result = nextDay+ CONCATENATION +monthTest+CONCATENATION+yearTest;
         return result;
     }
 }
