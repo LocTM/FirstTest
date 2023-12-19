748e9e633ee5de3ad49f0640f7b2d38dcf17fb03
LocTM <tranminhloc.anhk@gmail.com>
2023-12-19 10:26:10 +0700

    done test 31/1/2018

--- src/NextDayCalculator.java
+++ src/NextDayCalculator.java
@@ -3,8 +3,15 @@ public class NextDayCalculator {
     public static final String CONCATENATION = "/";
 
     public static String getNextDay(int dayTest, int monthTest, int yearTest) {
-        int nextDay = dayTest+1;
-        String result = nextDay+ CONCATENATION +monthTest+CONCATENATION+yearTest;
+        if (dayTest==31){
+            monthTest++;
+            dayTest=1;
+        }
+        else {
+            dayTest++;
+        }
+
+        String result = dayTest+ CONCATENATION +monthTest+CONCATENATION+yearTest;
         return result;
     }
 }
