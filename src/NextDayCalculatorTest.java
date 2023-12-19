e9395e68fa1c4a4fae21754d484512101981420a
LocTM <tranminhloc.anhk@gmail.com>
2023-12-19 10:23:17 +0700

    add test 31/1/2018

--- src/NextDayCalculatorTest.java
+++ src/NextDayCalculatorTest.java
@@ -18,4 +18,18 @@ public class NextDayCalculatorTest {
         assertEquals(result, expected);
     }
 
+    @Test
+    @DisplayName("case 31/1/2018")
+    public void testDay31Month1Year2018(){
+        //        chuan bi du lieu
+        int dayTest=31;
+        int monthTest=1;
+        int yearTest=2018;
+        String expected = "1/2/2018";
+//        thuc goi ham can test
+        String result = NextDayCalculator.getNextDay(dayTest, monthTest, yearTest);
+//        so sanh: ket qua thuc te voi ket qua mong muon
+        assertEquals(result, expected);
+    }
+
 }
