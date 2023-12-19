462e3d20790e56d7293bc59dcf1a6c352504f5cf
LocTM <tranminhloc.anhk@gmail.com>
2023-12-19 10:54:00 +0700

    pass test 30/4/2018

--- src/NextDayCalculator.java
+++ src/NextDayCalculator.java
@@ -3,7 +3,24 @@ public class NextDayCalculator {
     public static final String CONCATENATION = "/";
 
     public static String getNextDay(int dayTest, int monthTest, int yearTest) {
-        if (dayTest==31){
+//        neu thang 1-3-5-7-8-10-12->31
+//        neu thang con lai 30
+        int lastOfMonth=0;
+        switch (monthTest){
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                lastOfMonth=31;
+                break;
+            default:
+                lastOfMonth=30;
+        }
+
+        if (dayTest==lastOfMonth){
             monthTest++;
             dayTest=1;
         }
--- src/NextDayCalculatorTest.java
+++ src/NextDayCalculatorTest.java
@@ -31,5 +31,18 @@ public class NextDayCalculatorTest {
 //        so sanh: ket qua thuc te voi ket qua mong muon
         assertEquals(result, expected);
     }
+    @Test
+    @DisplayName("case 30/4/2018")
+    public void testDay30Month4Year2018(){
+        //        chuan bi du lieu
+        int dayTest=30;
+        int monthTest=4;
+        int yearTest=2018;
+        String expected = "1/5/2018";
+//        thuc goi ham can test
+        String result = NextDayCalculator.getNextDay(dayTest, monthTest, yearTest);
+//        so sanh: ket qua thuc te voi ket qua mong muon
+        assertEquals(result, expected);
+    }
 
 }
