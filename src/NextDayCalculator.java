7026772340e64f2c6da6781ee1019de589f7b33c
LocTM <tranminhloc.anhk@gmail.com>
2023-12-19 11:00:52 +0700

    main

--- src/NextDayCalculator.java
+++ src/NextDayCalculator.java
@@ -5,6 +5,21 @@ public class NextDayCalculator {
     public static String getNextDay(int dayTest, int monthTest, int yearTest) {
 //        neu thang 1-3-5-7-8-10-12->31
 //        neu thang con lai 30
+        int lastOfMonth = getLastOfMonth(monthTest);
+
+        if (dayTest==lastOfMonth){
+            monthTest++;
+            dayTest=1;
+        }
+        else {
+            dayTest++;
+        }
+
+        String result = dayTest+ CONCATENATION +monthTest+CONCATENATION+yearTest;
+        return result;
+    }
+
+    private static int getLastOfMonth(int monthTest) {
         int lastOfMonth=0;
         switch (monthTest){
             case 1:
@@ -19,16 +34,6 @@ public class NextDayCalculator {
             default:
                 lastOfMonth=30;
         }
-
-        if (dayTest==lastOfMonth){
-            monthTest++;
-            dayTest=1;
-        }
-        else {
-            dayTest++;
-        }
-
-        String result = dayTest+ CONCATENATION +monthTest+CONCATENATION+yearTest;
-        return result;
+        return lastOfMonth;
     }
 }
